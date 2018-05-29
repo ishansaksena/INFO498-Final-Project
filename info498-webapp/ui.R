759cdbff182a98cbfd0ac4dd26b51d7ccf5f85ea
Ishan Saksena <ishansaksena@gmail.com>
2018-05-29 16:01:29 -0700

    Added dataset blurbs

--- info498-webapp/ui.R
+++ info498-webapp/ui.R
@@ -22,10 +22,36 @@ shinyUI(fluidPage(
       # Add content for the About Us Tab
     
     ),
-    tabPanel("Data" # Add a comma here when writing code for this tab
+    tabPanel("Data", # Add a comma here when writing code for this tab
              
-             # Add content for the About Us Tab
-             
+        h2("500 Cities Health Data"),
+        p(
+          a(href="https://www.cdc.gov/500cities/", "The 500 cities health dataset"),
+          " provides city and census tract level estimates on chronic disease factors, health outcomes and 
+            clinical preventive services."
+        ),
+        
+        h2("Park Quality"),
+        p(
+          a(href="http://parkscore.tpl.org/rankings_advanced.php#sm.0000kbft6m124iel6ufsl45r54oqa", "The Parkscore 2018 dataset"),
+          " provides city level scores for the quality of parks measured by acreage, access, safety, amenities 
+          for the 100 most populous cities in the US. 93 of these cities also appear in 500 Cities health dataset."
+        ),
+        
+        h2("Walkability and Bikeability"),
+        p(
+          a(href="https://www.walkscore.com/cities-and-neighborhoods/", "The Walkability annd Bikeability dataset"),
+          " ranks cities based on walkability and bikeabilty. It scores both measures on the basis of access to public transit, 
+          distances to amenities, proximity to other people and places of interest. "
+        ),
+        
+        
+        h2("Food Access"),
+        p(
+          a(href="https://www.ers.usda.gov/data-products/food-access-research-atlas/", "The Food Access Dataset"),
+          " provides a census level overview for food access indicators and breaks it down further by race, age and income. 
+          It provides measures for how many people have access to a grocery store within half a mile, 1 mile, and 10 miles."
+        )
     ),
     
     tabPanel("Parks",
