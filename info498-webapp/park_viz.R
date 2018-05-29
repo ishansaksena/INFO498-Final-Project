314898bde5538b0f5eba480e3eca0f236811c520
zubinchopra <zubinc@uw.edu>
2018-05-29 09:48:41 -0700

    Deployed Shiny app

--- info498-webapp/park_viz.R
+++ info498-webapp/park_viz.R
@@ -6,7 +6,8 @@ park_data <- read.csv("prepped_data/ParkRankings.csv")
 
 park_data <- park_data[unique(park_data$City), ]
 park_data <- park_data[order(-park_data$ParkScore), ]
-park_data_viz <- park_data %>% select("City", "Arthritis.among.adults.aged...18.Years", "Binge.drinking.among.adults.aged...18.Years", "Coronary.heart.disease.among.adults.aged...18.Years", "Current.asthma.among.adults.aged...18.Years", "Current.smoking.among.adults.aged...18.Years", "High.blood.pressure.among.adults.aged...18.Years", "High.cholesterol.among.adults.aged...18.Years.who.have.been.screened.in.the.past.5.Years", "Mental.health.not.good.for...14.days.among.adults.aged...18.Years", "Obesity.among.adults.aged...18.Years", "Physical.health.not.good.for...14.days.among.adults.aged...18.Years", "ParkScore")
+cols <- c("City", "Arthritis.among.adults.aged...18.Years", "Binge.drinking.among.adults.aged...18.Years", "Coronary.heart.disease.among.adults.aged...18.Years", "Current.asthma.among.adults.aged...18.Years", "Current.smoking.among.adults.aged...18.Years", "High.blood.pressure.among.adults.aged...18.Years", "High.cholesterol.among.adults.aged...18.Years.who.have.been.screened.in.the.past.5.Years", "Mental.health.not.good.for...14.days.among.adults.aged...18.Years", "Obesity.among.adults.aged...18.Years", "Physical.health.not.good.for...14.days.among.adults.aged...18.Years", "ParkScore")
+park_data_viz <- park_data[, cols]
 colnames(park_data_viz) <- c("City", "Arthiritis", "Binge Drinking", "Coronary Heart Disease", "Asthma", "Smoking", "High Blood Pressure", "High Cholestrol", "Mental Health", "Obesity", "Physical Health", "Park Score")
 
 parkViz <- function(condition) {
@@ -28,7 +29,8 @@ parkViz <- function(condition) {
 }
 
 parkBarGraph <- function(condition) {
-  df <- head(park_data_viz[order(-park_data_viz[condition]), ] %>% select("City", condition), 10)
+  df <- head(park_data_viz[order(-park_data_viz[condition]), ], 10)
+  df <- df[, c("City", condition)]
   plot <- ggplot(df, aes(x = City, y = df[condition])) +
     geom_bar(stat = "identity", width = .5, fill = "lightcoral") + 
     labs(x = "City", y = paste0(condition, "(%)")) +
